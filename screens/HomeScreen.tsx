6b4a35337da75a42cbf2b704f8d661a966b83189
santhitak <63070175@kmitl.ac.th>
2022-11-26 21:37:49 +0700

    fix: unscrollable screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,55 +1,65 @@
-import { SafeAreaView } from "react-native";
-import { Text, Button, Icon, Div, Input, Image } from "react-native-magnus";
+import { SafeAreaView, ScrollView } from "react-native";
+import {
+  Text,
+  Button,
+  Icon,
+  Div,
+  Input,
+  Image,
+  ScrollDiv,
+} from "react-native-magnus";
 import ButtonCategories from "../components/Home/ButtonCategories";
 import HomeProduct from "../components/HomeProduct";
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Div w={"100vw"} minH={"100vh"} bg={"white"}>
-        <Div p={20}>
-          <Div row>
-            <Input
-              placeholder="Search"
-              p={10}
-              w={"100%"}
-              focusBorderColor="blue700"
-              bg="#EEE"
-              color="gray700"
-              borderColor="gray200"
-              prefix={
-                <Icon name="search" color="gray500" fontFamily="Feather" />
-              }
-            />
+    <ScrollView>
+      <SafeAreaView>
+        <Div w={"100vw"} minH={"100vh"} bg={"white"}>
+          <Div p={20}>
+            <Div row>
+              <Input
+                placeholder="Search"
+                p={10}
+                w={"100%"}
+                focusBorderColor="blue700"
+                bg="#EEE"
+                color="gray700"
+                borderColor="gray200"
+                prefix={
+                  <Icon name="search" color="gray500" fontFamily="Feather" />
+                }
+              />
+            </Div>
+            <Div row>
+              <ButtonCategories />
+            </Div>
+            <Div row>
+              <Input
+                placeholder="สร้างโพสต์ใหม่"
+                p={10}
+                my={10}
+                w="100%"
+                bg="#EEE"
+                color="gray700"
+                fontWeight="bold"
+                borderColor="gray200"
+                suffix={
+                  <Icon name="edit" color="gray500" fontFamily="AntDesign" />
+                }
+              />
+            </Div>
           </Div>
-          <Div row>
-            <ButtonCategories />
+          <Div row my={10} mx={15}>
+            <Text fontWeight="bold" fontSize="4xl">
+              ใหม่ล่าสุด
+            </Text>
           </Div>
-          <Div row>
-            <Input
-              placeholder="สร้างโพสต์ใหม่"
-              p={10}
-              my={10}
-              w="100%"
-              bg="#EEE"
-              color="gray700"
-              fontWeight="bold"
-              borderColor="gray200"
-              suffix={
-                <Icon name="edit" color="gray500" fontFamily="AntDesign" />
-              }
-            />
+          <Div>
+            <HomeProduct />
           </Div>
         </Div>
-        <Div row my={10} mx={15}>
-          <Text fontWeight="bold" fontSize="4xl">
-            ใหม่ล่าสุด
-          </Text>
-        </Div>
-        <Div>
-          <HomeProduct />
-        </Div>
-      </Div>
-    </SafeAreaView>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 export default HomeScreen;

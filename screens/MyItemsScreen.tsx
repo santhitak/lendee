94b8e28564aeb0f18d837a84c53b08a1922742a2
santhitak <63070175@kmitl.ac.th>
2022-12-05 05:52:18 +0700

    feat: status update

--- screens/MyItemsScreen.tsx
+++ screens/MyItemsScreen.tsx
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text, Div, Button, Image } from "react-native-magnus";
 import { NoItems, Container } from "../components";
-import { ProductImageTypes, ProductTypes } from "../constants";
+import {
+  FavoriteProductTypes,
+  ProductImageTypes,
+  ProductTypes,
+} from "../constants";
 
 const MyItemsScreen = ({ navigation }: any) => {
   const [item, setItem] = useState<String[]>();
   const [img, setImg] = useState<ProductImageTypes[]>([]);
+  const [lend, setLend] = useState<FavoriteProductTypes[]>([]);
 
   useState(() => {
     fetch("http://localhost:3000/products/1")
@@ -32,6 +37,17 @@ const MyItemsScreen = ({ navigation }: any) => {
       });
   });
 
+  useState(() => {
+    fetch("http://localhost:3000/lend/1")
+      .then((response) => response.json())
+      .then((json) => {
+        setLend(json);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
   return (
     <Container>
       <Text fontSize={28} fontWeight={"bold"}>
@@ -46,39 +62,51 @@ const MyItemsScreen = ({ navigation }: any) => {
                   if (item.authorId === 1) {
                     return (
                       <Div key={i}>
-                        <Div
-                          row
-                          alignItems="center"
-                          justifyContent="flex-start"
-                        >
-                          {img.map(
-                            (itemImage: ProductImageTypes, i: number) => {
-                              if (itemImage.productId === item.id) {
-                                return (
-                                  <Div
-                                    h={80}
-                                    w={80}
-                                    rounded={20}
-                                    bgImg={{
-                                      uri: itemImage.img,
-                                    }}
-                                    bgMode="cover"
-                                    key={i}
-                                  />
-                                );
-                              }
-                            }
-                          )}
-                          <Div my={15} ml={20}>
-                            <Text my={5} fontWeight="bold">
-                              {item.productName}
-                            </Text>
-                            <Button my={5} bg="#1F4492" color="white">
-                              ดูรายละเอียด
-                            </Button>
-                          </Div>
-                        </Div>
-                        <Text color="#9e9e9e">สถานะ: ว่าง</Text>
+                        {lend.map((data: FavoriteProductTypes, i: number) => {
+                          return (
+                            <>
+                              <Div
+                                row
+                                alignItems="center"
+                                justifyContent="flex-start"
+                                key={i}
+                              >
+                                {img.map(
+                                  (itemImage: ProductImageTypes, i: number) => {
+                                    if (itemImage.productId === item.id) {
+                                      return (
+                                        <Div
+                                          h={80}
+                                          w={80}
+                                          rounded={20}
+                                          bgImg={{
+                                            uri: itemImage.img,
+                                          }}
+                                          bgMode="cover"
+                                          key={i}
+                                        />
+                                      );
+                                    }
+                                  }
+                                )}
+                                <Div my={15} ml={20}>
+                                  <Text my={5} fontWeight="bold">
+                                    {item.productName}
+                                  </Text>
+                                  <Button my={5} bg="#1F4492" color="white">
+                                    ดูรายละเอียด
+                                  </Button>
+                                </Div>
+                              </Div>
+                              <Text color="#9e9e9e">
+                                สถานะ:{" "}
+                                {data.productId === item.id
+                                  ? "กำลังยืม"
+                                  : "ว่าง"}
+                              </Text>
+                            </>
+                          );
+                        })}
                       </Div>
                     );
                   }

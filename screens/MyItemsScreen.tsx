ce10634874ed77e9475192d87204ad0b662a6035
santhitak <63070175@kmitl.ac.th>
2022-12-05 06:18:26 +0700

    feat: lend items

--- screens/MyItemsScreen.tsx
+++ screens/MyItemsScreen.tsx
@@ -8,45 +8,134 @@ import {
   ProductTypes,
 } from "../constants";
 
+const lendData = [
+  {
+    id: 2,
+    userId: 1,
+    productId: 14,
+  },
+];
+
+const itemData = [
+  {
+    id: 1,
+    createdAt: "2022-12-03T03:48:24.048Z",
+    email: "member@kmitl.ac.th",
+    name: "Danupa Tangjaidee",
+    phone: "0815467894",
+    product: [
+      {
+        id: 4,
+        createdAt: "2022-12-03T13:23:47.660Z",
+        authorId: 1,
+        productName: "EKTORP Sofa",
+        productDetail:
+          "เอียคทอร์ปโซฟาจากอิเกีย ให้ยืมเช่าเพื่อถ่ายงานครับ สภาพดี\r\n",
+        productCost: 899,
+        productQuantity: 2,
+        productUrl: "",
+        isFavorite: true,
+      },
+      {
+        id: 13,
+        createdAt: "2022-12-03T13:23:47.834Z",
+        authorId: 1,
+        productName: "ALSTERN อัลสเติร์น",
+        productDetail:
+          "ALSTERN อัลสเติร์น\r\nพรมผ้าเทอร์รีผืนนี้จะกลายเป็นขวัญใจคุณได้ไม่ยาก ด้วยความที่เข้ากันกับผ้าเช็ดตัวหลายรุ่นของเราได้ดี\r\n159\r\n",
+        productCost: 159,
+        productQuantity: 2,
+        productUrl: "",
+        isFavorite: false,
+      },
+      {
+        id: 14,
+        createdAt: "2022-12-03T13:23:48.001Z",
+        authorId: 1,
+        productName: "RIBBA ริบบ้า",
+        productDetail:
+          "RIBBA ริบบ้า\r\nให้คุณเนรมิตผนังห้องเป็นแกลเลอรีภาพขนาดใหญ่ได้สบาย แผ่นปิดหน้ากรอบรูปทำจากพลาสติกใส จึงใช้งานได้อย่างปลอดภัย\r\n",
+        productCost: 99,
+        productQuantity: 3,
+        productUrl: "",
+        isFavorite: false,
+      },
+    ],
+  },
+];
+
+const imgData = [
+  {
+    id: 1,
+    img: "https://www.ikea.com/th/th/images/products/ektorp-3-seat-sofa-totebo-light-beige__0818572_pe774494_s5.jpg",
+    productId: 4,
+  },
+  {
+    id: 2,
+    img: "https://www.ikea.com/th/th/images/products/ektorp-3-seat-sofa-totebo-light-beige__0818572_pe774494_s5.jpg",
+    productId: 14,
+  },
+  {
+    id: 4,
+    img: "https://www.ikea.com/th/th/images/products/alstern-bath-mat-dark-grey__1092864_pe863108_s5.jpg",
+    productId: 13,
+  },
+  {
+    id: 9,
+    img: "https://cdn.homepro.co.th/ART_IMAGE/11/076/1107603/447x447/10072021_1107603$Imagec1.jpg",
+    productId: 32,
+  },
+  {
+    id: 11,
+    img: "https://img.kaidee.com/prd/20190707/348648729/b/e9cd1142-e862-4eae-9fc7-c7cedb5d0087.jpg",
+    productId: 33,
+  },
+  {
+    id: 12,
+    img: "https://static.mercdn.net/item/detail/orig/photos/m83023938807_1.jpg",
+    productId: 34,
+  },
+];
+
 const MyItemsScreen = ({ navigation }: any) => {
-  const [item, setItem] = useState<String[]>();
-  const [img, setImg] = useState<ProductImageTypes[]>([]);
-  const [lend, setLend] = useState<FavoriteProductTypes[]>([]);
+  const [item, setItem] = useState(itemData);
+  const [img, setImg] = useState<ProductImageTypes[]>(imgData);
+  const [lend, setLend] = useState<FavoriteProductTypes[]>(lendData);
 
-  useState(() => {
-    fetch("http://localhost:3000/products/1")
-      .then((response) => response.json())
-      .then((json) => {
-        const itemArr = [];
-        itemArr.push(json);
-        setItem(itemArr);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  });
+  // useState(() => {
+  //   fetch("http://localhost:3000/products/1")
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       const itemArr = [];
+  //       itemArr.push(json);
+  //       setItem(itemArr);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // });
 
-  useState(() => {
-    fetch("http://localhost:3000/productImages")
-      .then((response) => response.json())
-      .then((json) => {
-        setImg(json);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  });
+  // useState(() => {
+  //   fetch("http://localhost:3000/productImages")
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setImg(json);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // });
 
-  useState(() => {
-    fetch("http://localhost:3000/lend/1")
-      .then((response) => response.json())
-      .then((json) => {
-        setLend(json);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  });
+  // useState(() => {
+  //   fetch("http://localhost:3000/lend/1")
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setLend(json);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // });
 
   return (
     <Container>
